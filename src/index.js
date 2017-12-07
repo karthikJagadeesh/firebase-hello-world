a78f7f6ceb797c814f0871a389fbe3847e1aa7c3
Karthik Jagadeesha <kartu8@gmail.com>
2017-12-07 23:48:56 +0530

    Add firebase, some p5 interactions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
 import "babel-polyfill";
 import p5 from "p5";
+import firebase from "firebase";
+import config from "./firebaseKeys.js";
 
 const app = p => {
-  p.draw = _ => {};
+  let database = null;
+  let ref = null;
+  let balls = [];
+
+  const createStaggeringBall = (posX, posY) => {
+    p.noFill();
+    p.stroke(255);
+    p.ellipse(posX, posY, 20, 20);
+  }
+
+  p.draw = _ => {
+    p.background(0);
+    balls.forEach(ball => {
+      createStaggeringBall(ball.x, ball.y)
+    })
+  };
 
   p.setup = _ => {
+    firebase.initializeApp(config);
+    database = firebase.database();
+    ref = database.ref("ballsCount");
+
     p.createCanvas(p.windowWidth, p.windowHeight);
     p.background(0);
+  };
+
+  p.mousePressed = event => {
+    event.preventDefault();
+    event.stopPropagation();
+    if (p.mouseButton === p.LEFT) {
+      balls.push({x: p.mouseX, y: p.mouseY});
+    } else if (p.mouseButton === p.RIGHT) {
+      balls.pop();
+    }
+  };
+
+  p.windowResized = _ => {
+    p.resizeCanvas(p.windowWidth, p.windowHeight);
   };
 };
 
